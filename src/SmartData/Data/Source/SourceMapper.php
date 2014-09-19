2a4684312a80ee6f32a5c52ff810356b7b78b74c
Gabriel Bull <me@gabrielbull.com>
2014-09-19 16:07:03 -0400

    Skip SSL validation

--- src/SmartData/Data/Source/SourceMapper.php
+++ src/SmartData/Data/Source/SourceMapper.php
@@ -8,7 +8,17 @@ class SourceMapper
      */
     public function loadCollection()
     {
-        $collection = json_decode(file_get_contents("https://smartdataprovider.com/source.json"), true);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_URL, "https://smartdataprovider.com/source.json");
+        curl_setopt($ch, CURLOPT_SSLVERSION, 3);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $result = curl_exec($ch);
+        curl_close($ch);
+
+        $collection = json_decode($result, true);
+
         return $this->mapJsonCollection($collection);
     }
 
