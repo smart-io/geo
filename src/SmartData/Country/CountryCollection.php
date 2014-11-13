ba3ad60a1b2314bc8ab41f72c26790dadecec3e4
Gabriel Bull <me@gabrielbull.com>
2014-11-13 13:38:35 -0500

    Added findAll to country repo

--- src/SmartData/Country/CountryCollection.php
+++ src/SmartData/Country/CountryCollection.php
@@ -4,6 +4,9 @@ namespace SmartData\SmartData\Country;
 use SmartData\SmartData\ArrayCollection;
 use SmartData\SmartData\Coordinate\CoordinateCollectionInterface;
 
+/**
+ * @method CountryEntity get($key)
+ */
 class CountryCollection extends ArrayCollection implements CoordinateCollectionInterface
 {
 }
--- src/SmartData/Country/CountryRepository.php
+++ src/SmartData/Country/CountryRepository.php
@@ -8,9 +8,19 @@ class CountryRepository
     const COUNTRIES_FILES = 'countries/countries/%s.json';
 
     /**
+     * @var CountryCollection
+     */
+    private $container;
+
+    /**
      * @var Storage
      */
     private $storage;
+
+    /**
+     * @var CountryMapper
+     */
+    private $mapper;
 
     /**
      * @param Storage $storage
@@ -18,6 +28,28 @@ class CountryRepository
     public function __construct(Storage $storage)
     {
         $this->storage = $storage;
+    }
+
+    /**
+     * @return CountryMapper
+     */
+    private function getMapper()
+    {
+        if (null === $this->mapper) {
+            $this->mapper = new CountryMapper();
+        }
+        return $this->mapper;
+    }
+
+    /**
+     * @return CountryCollection
+     */
+    public function findAll()
+    {
+        if (null === $this->container) {
+            $this->container = $this->getMapper()->loadCollection();
+        }
+        return $this->container;
     }
 
     /**
