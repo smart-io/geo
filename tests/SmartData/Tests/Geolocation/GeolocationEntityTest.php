9d7b3ef9de5e55be70a9528c385d53d61c0e6f5f
Gabriel Bull <me@gabrielbull.com>
2014-11-18 20:28:37 -0500

    Fixed unit test when not in canada

--- tests/SmartData/Tests/Geolocation/GeolocationEntityTest.php
+++ tests/SmartData/Tests/Geolocation/GeolocationEntityTest.php
@@ -4,13 +4,15 @@ namespace SmartData\SmartData\Tests\Geolocation;
 use PHPUnit_Framework_TestCase;
 use SmartData\SmartData\Country\CountryEntity;
 use SmartData\SmartData\Geolocation\GeolocationRepository;
+use SmartData\SmartData\Ip\IpEntity;
 use SmartData\SmartData\Ip\IpRepository;
 
 class GeolocationEntityTest extends PHPUnit_Framework_TestCase
 {
     public function testGetCountry()
     {
-        $ip = (new IpRepository())->findRealIp();
+        $ip = new IpEntity();
+        $ip->setIp('70.80.14.120');
 
         $repository = new GeolocationRepository();
         $geolocation = $repository->findByIp($ip);
