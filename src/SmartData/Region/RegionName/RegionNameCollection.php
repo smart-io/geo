7b71d41dc2cc3e98d852328771a1322f74a55582
Gabriel Bull <me@gabrielbull.com>
2014-12-10 15:53:37 -0500

    Added region name entity

--- src/SmartData/Region/RegionName/RegionNameCollection.php
+++ src/SmartData/Region/RegionName/RegionNameCollection.php
@@ -2,7 +2,40 @@
 namespace SmartData\SmartData\Region\RegionName;
 
 use SmartData\SmartData\ArrayCollection;
+use SmartData\SmartData\Language\LanguageEntity;
 
 class RegionNameCollection extends ArrayCollection
 {
+    /**
+     * @var array|RegionNameEntity[]
+     */
+    protected $elements;
+
+    /**
+     * @param string|LanguageEntity $key
+     * @return null|RegionNameEntity
+     */
+    public function get($key)
+    {
+        if (isset($this->elements[$key])) {
+            return $this->elements[$key];
+        } else {
+            if (is_string($key)) {
+                foreach ($this->elements as $element) {
+                    if (is_string($element->getLanguage()) && $key === $element->getLanguage()) {
+                        return $element;
+                    } elseif ($key === $element->getLanguage()->getCode()) {
+                        return $element;
+                    }
+                }
+            } elseif ($key instanceof LanguageEntity) {
+                foreach ($this->elements as $element) {
+                    if ($key === $element->getLanguage()) {
+                        return $element;
+                    }
+                }
+            }
+        }
+        return null;
+    }
 }
