4271b7697db983885e77c9188f8f5444b64accca
Gabriel Bull <me@gabrielbull.com>
2015-05-16 14:47:14 -0400

    Added caching of real ip dev method

--- src/Ip/IpRepository.php
+++ src/Ip/IpRepository.php
@@ -26,11 +26,16 @@ class IpRepository
      */
     public function findRealIp()
     {
-        $handle = curl_init();
-        curl_setopt($handle, CURLOPT_URL, 'http://icanhazip.com');
-        curl_setopt($handle, CURLOPT_RETURNTRANSFER, 1);
-        $ip = trim(curl_exec($handle));
-        curl_close($handle);
+        if (extension_loaded('apc') && ini_get('apc.enabled') && apc_exists('SMART_GEO_REAL_IP')) {
+            $ip = apc_fetch('SMART_GEO_REAL_IP');
+        } else {
+            $handle = curl_init();
+            curl_setopt($handle, CURLOPT_URL, 'http://icanhazip.com');
+            curl_setopt($handle, CURLOPT_RETURNTRANSFER, 1);
+            $ip = trim(curl_exec($handle));
+            curl_close($handle);
+            apc_store('SMART_GEO_REAL_IP', $ip);
+        }
 
         if (!empty($ip) && preg_match('/^[\d\.]*$/', $ip)) {
             return (new IpMapper())->mapArrayEntity(['ip' => $ip]);
